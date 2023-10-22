01929691076cba92b912d17ff0b514cf330f1b95
David Bracewell <david@davidbracewell.com>
2023-10-22 12:48:45 -0500

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,71 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from PIL import Image
+import tensorflow as tf
+from keras import Sequential
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
+from keras.preprocessing import image as kimage
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+base_model = tf.keras.applications.EfficientNetB2(
+    weights="imagenet", input_shape=(224, 224, 3), include_top=False
+)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+for layer in base_model.layers:
+    layer.trainable = False
+model = Sequential()
+model.add(base_model)
+# model.add(GaussianNoise(0.25))
+model.add(GlobalAveragePooling2D())
+model.add(Dense(256, activation="relu"))
+model.add(BatchNormalization())
+# model.add(GaussianNoise(0.25))
+model.add(Dropout(0.25))
+model.add(Dense(4, activation="softmax"))
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+model.compile(
+    loss="categorical_crossentropy",
+    optimizer="adam",
+    metrics=["accuracy", "AUC", "Precision", "Recall"],
+)
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def main():
+    st.title("Cancer Identification")
+    uploaded_file = st.file_uploader("Choose a image file", type=["jpg", "png"])
+
+    if uploaded_file is not None:
+        filepath = "best_model.h5"
+        model.load_weights(filepath)
+        image = Image.open(uploaded_file)
+        
+        col1, col2 = st.columns( [0.5, 0.5])
+        with col1:
+            st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+            st.image(image,width=300)  
+
+        with col2:
+            img = kimage.load_img(uploaded_file,target_size=(224,224))
+            imag = kimage.img_to_array(img)
+            imaga = np.expand_dims(imag,axis=0) 
+            ypred = model.predict(imaga)
+            a=np.argmax(ypred,-1)
+            if a==0:
+                op="Adenocarcinoma"
+            elif a==1:
+                op="large cell carcinoma"
+            elif a==2:
+                op="normal (void of cancer)"
+            else:
+                op="squamous cell carcinoma" 
+
+            
+            st.markdown(f'<h1 style="text-align: center">{op} ({ypred[0][a].item()*100:.1f}%)</h1>',unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
